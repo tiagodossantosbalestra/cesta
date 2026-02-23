9d594a503dffe15b6e8ad7e66c8685ba0943e07f
Tiago <tiago.amancio@escola.pr.gov.br>
2026-02-23 12:18:34 +0000

    4.2

--- src/telas/csta.js
+++ src/telas/csta.js
@@ -6,6 +6,13 @@ import topo from'../../assets/topo.png';
 export default function Cesta(){
     return<>
         <Image source={topo} />
+        <Text>Detalhes da cesta</Text>
+        <Text>Cesta de verduras</Text>
+        <Text>Jenny Jack Farm</Text>
+        <Text>Uma Cesta com produtos selecionados cuidadosamente da fazenda direto para sua cozinha.</Text>
+        <Text>R$: 40,00</Text>
+
+
     <text>olá</text>
     
     
