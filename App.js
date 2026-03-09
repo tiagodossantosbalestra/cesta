d2b6bc4c79141d8573b20aec2d1c917d5d02fe13
Tiago <tiago.amancio@escola.pr.gov.br>
2026-03-09 12:39:42 +0000

    4.6

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
-import { Text} from 'react-native';
-import { SafeAreaView } from 'react-native-web';
+import {View} from 'react-native';
 import Cesta from './src/telas/Cesta';
+import { SafeAreaView } from 'react-native-web';
+import{
+        useFonts,
+        montserrat_400Regular
+        montserrat_700Bold
+      } from '@expo-google-fonts/montserrat';
 
+      
 export default function App() {
   return (
     <SafeAreaView>
